477239af25d877b896b553d6189c612c9503d07b
Francois Perron <fperron@gmail.com>
2013-12-20 23:37:43 -0500

    Hello world

---
+++ yose-csharp/Application.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yose
+{
+    static class Application
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Hello World!");
+        }
+    }
+}
