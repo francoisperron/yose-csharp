9ff501e83f9b4f382a9dbfd3d7b419700ca4c99e
Francois Perron <fperron@gmail.com>
2013-12-24 12:39:12 -0500

    Script to download nuget certificates

---
+++ nuget/setup-certificates.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo mozroots --import --machine --sync
+sudo certmgr -ssl -m https://go.microsoft.com
+sudo certmgr -ssl -m https://nugetgallery.blob.core.windows.net
+sudo certmgr -ssl -m https://nuget.org
+
